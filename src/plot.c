db116dc4057ff82a130520ff8c157a21426f9733
eco <spambaconspam@protonmail.com>
2022-02-17 19:12:53 +0100

    renamed viewport to plot

---
+++ src/plot.c
@@ -0,0 +1,269 @@
+#include "plot.h"
+
+
+void pl_print(Plot* pl)
+{
+    for (int y=pl->ysize-1 ; y>=0 ; y--) {
+        for (int x=0 ; x<pl->xsize ; x++) {
+            printf("%d%s%s",pl->cells[x][y]->fgcol, pl->cells[x][y]->chr, RESET);
+        }
+        printf("\n");
+    }
+}
+
+//void pl_draw_candlestick(Plot* pl, uint32_t ix, uint32_t iopen, uint32_t ihigh, uint32_t ilow, uint32_t iclose)
+void pl_draw_candlestick(Plot* pl, uint32_t ix, int32_t iopen, int32_t ihigh, int32_t ilow, int32_t iclose)
+{
+    //set_status(1, "%d %d %d %d %d", ix, iopen, ihigh, ilow, iclose);
+    /* draw one candlestick in viewport */
+    // GREEN
+    //ilow = (ilow < 0) ? 0 : ilow;
+    //ihigh = (ihigh > pl->ysize-1) ? pl->ysize-1 : ihigh;
+
+    if (iopen < iclose) {
+        for (int y=ilow ; y<=ihigh ; y++) {
+            if (y < pl->pystart || y > pl->ysize-1)
+                continue;
+            Cell* cell = pl_get_cell(pl, ix, y);
+            strcpy(cell->chr, CS_WICK);
+            cell->fgcol = GREEN;
+        }
+        for (int y=iopen ; y<=iclose ; y++) {
+            if (y < pl->pystart || y > pl->ysize-1)
+                continue;
+            Cell* cell = pl_get_cell(pl, ix, y);
+            strcpy(cell->chr, CS_BODY);
+            cell->fgcol = GREEN;
+        }
+
+    // RED
+    }
+    else {
+        for (int y=ilow ; y<=ihigh ; y++) {
+            if (y < pl->pystart || y > pl->ysize-1)
+                continue;
+            Cell* cell = pl_get_cell(pl, ix, y);
+            strcpy(cell->chr, CS_WICK);
+            cell->fgcol = RED;
+        }
+        for (int y=iclose ; y<=iopen ; y++) {
+            if (y < pl->pystart || y > pl->ysize-1)
+                continue;
+            Cell* cell = pl_get_cell(pl, ix, y);
+            strcpy(cell->chr, CS_BODY);
+            cell->fgcol = RED;
+        }
+    }
+}
+
+void pl_draw_candlesticks(Plot* pl, Group* g, double dmin, double dmax, int32_t yoffset)
+{
+    /* itter groups and draw them in viewport */
+    // start at this xy indices
+    uint32_t ix = pl->pxstart;
+    uint32_t iy = pl->pystart;
+
+    
+    uint32_t goffset = pl->xsize - pl->pxsize;
+    while (goffset != 0) {
+        g = g->next;
+        goffset--;
+    }
+
+    while (g != NULL) {
+        if (! g->is_empty) {
+
+            // map data point from data range to terminal rows range
+            uint32_t iopen  = map(g->open,  dmin, dmax, iy, pl->ysize-1) + yoffset;
+            uint32_t ihigh  = map(g->high,  dmin, dmax, iy, pl->ysize-1) + yoffset;
+            uint32_t ilow   = map(g->low,   dmin, dmax, iy, pl->ysize-1) + yoffset;
+            uint32_t iclose = map(g->close, dmin, dmax, iy, pl->ysize-1) + yoffset;
+
+            pl_draw_candlestick(pl, ix, iopen, ihigh, ilow, iclose);
+        }
+        g = g->next;
+        ix++;
+    }
+}
+
+void pl_draw_last_data(Plot* pl, double dmin, double dmax, double pany, double lasty)
+{
+    /* highlight last data in axis */
+
+    // calculate first column of axis
+    //uint32_t xstart = pl->xsize - pl->ryaxis_size -1;
+    uint32_t xstart = pl->ryaxis_start;
+
+    // calculate y index of last data
+    int32_t ilasty = map(lasty, dmin, dmax, pl->pystart, pl->ysize-1) + pany;
+
+    char buf[50] = {'\0'};
+    get_tickerstr(buf, lasty, pl->ryaxis_size, pl->ryaxis_nwhole, pl->ryaxis_nfrac);
+    char* ptr = buf;
+
+    // if last data is out of range, stick to top/bottom
+    if (ilasty < pl->pystart)
+        ilasty = pl->pystart;
+    if (ilasty >= pl->ysize)
+        ilasty = pl->ysize-1;
+
+    for (int32_t ix=xstart ; ix<xstart+strlen(buf) ; ix++, ptr++) {
+        if (ix >= pl->xsize)
+            break;
+        Cell* c = pl_get_cell(pl, ix, ilasty);
+        c->chr[0] = *ptr;
+        c->fgcol = GREEN;
+    }
+    // draw line
+    for (int32_t ix=pl->pxstart ; ix<pl->pxstart+pl->pxsize ; ix++, ptr++) {
+        Cell* c = pl_get_cell(pl, ix, ilasty);
+        if (c->chr[0] == ' ') {
+            strcpy(c->chr, LINE_CHR);
+            c->fgcol = MAGENTA;
+        }
+    }
+}
+
+void pl_draw_xaxis(Plot* pl, Group* g)
+{
+    int32_t ix = pl->xaxis_xstart;
+
+    while (g != NULL) {
+        if (g->id % XTICK_SPACING == 0) {
+
+            char t[30] = {'\0'};
+            sprintf(t, "%5.2f", g->wstart);
+            char* tptr = t;
+
+            for (uint32_t x=ix ; x<ix+strlen(t) ; x++, tptr++) {
+                if (x >= pl->xaxis_xstart+pl->xaxis_xsize)
+                    break;
+
+                Cell* c = pl_get_cell(pl, x, pl->xaxis_ystart);
+                c->chr[0] = *tptr;
+            }
+        }
+        g = g->next;
+        ix++;
+    }
+}
+
+Cell* pl_get_cell(Plot* pl, uint32_t x, uint32_t y)
+{
+    return pl->cells[x][y];
+}
+
+Plot* pl_init(uint32_t xsize, uint32_t ysize)
+{
+    Plot* pl = malloc(sizeof(Plot));
+
+    pl->xsize = xsize;
+    pl->ysize = ysize;
+
+    pl->status_size = 2;
+    pl->lyaxis_size = 0;
+    pl->ryaxis_size = 15;
+    pl->xaxis_ysize = 1;
+
+    int x, y;
+
+    // TODO do this in one malloc call: pl->cells = malloc(sizeof(Cell*[pl->xsize][pl->ysize]));
+    pl->cells = malloc(pl->xsize * sizeof(Cell**));
+
+    for (x=0 ; x<pl->xsize ; x++) {
+        pl->cells[x] = malloc(pl->ysize * sizeof(Cell*));
+
+        for (y=0 ; y<pl->ysize ; y++) {
+            Cell* cell = malloc(sizeof(Cell));
+            cell->chr = strdup(EMPTY);
+            cell->fgcol = WHITE;
+            cell->x = x;
+            cell->y = y;
+            pl->cells[x][y] = cell;
+        }
+    }
+    return pl;
+}
+
+void pl_draw_ryaxis(Plot* pl, double dmin, double dmax, int32_t yoffset)
+{
+    /* calculate axis values and draw them in matrix.
+     * Also highlight last value of last point (not group)
+     */
+    // calculate stepsize between tickers
+    double step = (dmax - dmin) / pl->pxsize;
+
+    // calculate first column of axis
+    uint32_t xstart = pl->ryaxis_start;
+
+    // TODO create funtion that resets all dimensions of components
+    //
+    for (int32_t iy=pl->pystart ; iy<pl->ysize ; iy++) {
+
+        uint32_t ix;
+        char buf[50] = {'\0'};
+        double ticker = dmin + ((iy - yoffset)*step);
+        get_tickerstr(buf, ticker, pl->ryaxis_size, pl->ryaxis_nwhole, pl->ryaxis_nfrac);
+        char* pbuf = buf;
+
+        for (ix=xstart ; ix<xstart+strlen(buf) ; ix++, pbuf++) {
+            Cell* c = pl_get_cell(pl, ix, iy);
+            c->chr[0] = *pbuf;
+            c->fgcol = WHITE;
+        }
+    }
+}
+
+void pl_set_dimensions(Plot* pl, double dmin, double dmax)
+{
+    /* calculate all dimensions and start indices */
+    uint8_t spacer = 1;
+
+    pl->ryaxis_nfrac  = find_nfrac((dmax - dmin)); // digits behind the comma
+    //pl->ryaxis_nfrac  = find_nfrac((dmax - dmin)/pl->pysize); // digits behind the comma
+    pl->ryaxis_nwhole = find_nwhole(dmax); // digits before the comma
+    pl->ryaxis_size   = pl->ryaxis_nwhole + 1 + pl->ryaxis_nfrac;
+    pl->ryaxis_start  = pl->xsize - pl->ryaxis_size;
+
+    pl->pysize = pl->ysize - pl->status_size - pl->xaxis_ysize;
+    pl->pxsize = pl->xsize - pl->lyaxis_size - pl->ryaxis_size - spacer; // spacer for ryaxis
+
+    pl->xaxis_xsize = pl->pxsize;
+    pl->xaxis_xstart = pl->lyaxis_size;
+    pl->xaxis_ystart = pl->status_size;
+
+    pl->pxstart = pl->lyaxis_size;
+    pl->pystart = pl->status_size + pl->xaxis_ysize;
+}
+
+void pl_draw(Plot* pl, Index* index, Groups* groups, State* s)
+{
+    /* Draw all elements to internal matrix */
+    pl_set_dimensions(pl, s->dmin, s->dmax);
+    pl_draw_ryaxis(pl, s->dmin, s->dmax, s->pany);
+    pl_draw_candlesticks(pl, groups->group, s->dmin, s->dmax, s->pany);
+    pl_draw_last_data(pl, s->dmin, s->dmax, s->pany, (*index->ptail)->close);
+    pl_draw_xaxis(pl, groups->group);
+}
+
+void get_tickerstr(char* buf, double ticker, uint32_t ntotal, uint32_t nwhole, uint32_t nfrac)
+{
+    // TODO null terminate characters in cell
+    //
+    /* create ticker with specific amount of decimals and copy to string */
+    char* pbuf = buf;
+    char tmp[50] = {'\0'};
+    char sfrac[50] = {'\0'};
+
+    sprintf(tmp, "%d.", abs(ticker));
+    sprintf(sfrac, "%g", ticker - abs(ticker));
+    strncat(tmp, sfrac+2, nfrac);
+
+    for (int i=0; i<ntotal-strlen(tmp) ; i++)
+        strncat(buf, " ", 1);
+
+    strncat(buf, tmp, strlen(tmp));
+    set_status(1, "ry: %d[%d.%d] %d %d >>%s<<", ntotal, nwhole, nfrac, strlen(tmp), ntotal-strlen(tmp), buf);
+
+}
+
---
+++ src/plot.h
@@ -0,0 +1,110 @@
+#ifndef PLOT_H
+#define PLOT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "assert.h"
+
+#include "utils.h"
+#include "index.h"
+
+#define CS_BODY "█"
+#define CS_WICK "┃"
+#define EMPTY   " "
+#define LINE_CHR "─"
+
+#define RED   1
+#define GREEN 2
+#define WHITE 7
+#define MAGENTA 5
+//#define RED   "\033[0;31m"
+//#define GREEN "\033[0;32m"
+#define RESET "\033[0m"
+
+// data dimensions must have a middle point so should be an odd number
+#define MATRIX_WIDTH  1001
+#define MATRIX_HEIGHT 1001
+
+#define RAXIS_SIZE 5
+
+// chars inbetween tickers on x axis
+#define XTICK_SPACING 10
+
+// Node struct represents a cell in the matrix
+typedef struct Cell Cell;
+typedef struct Plot Plot;
+
+// Represents a cell in the matrix and viewport
+struct Cell {
+    int32_t x;
+    int32_t y;
+
+    char* chr;
+    uint16_t bgcol;
+    uint16_t fgcol;
+
+    // keep changed cells in linked list for easy updating
+    Cell* prev;
+    Cell* next;
+};
+
+// Represents the nodes that are visible on screen
+// Holds a subset of Matrix->nodes,
+struct Plot {
+    // total area dimensions
+    int xsize;
+    int ysize;
+
+    // plot area dimensions
+    int pxsize;
+    int pysize;
+
+    // coordinates of bottom left corner of plot area
+    int pxstart;
+    int pystart;
+
+    // 2d array representing data on screen
+    // every index has a cell struct
+    // if there are more than one cells (linked list) it means
+    // that we have lines drawing on top of other lines
+    // In this case do something cool with color and chars
+    Cell*** cells;
+
+    // width of axis
+    int lyaxis_size;
+    int ryaxis_size;
+
+    int ryaxis_nfrac;       // digits after decimal point
+    int ryaxis_nwhole;      // digits before decimal point
+
+    int lyaxis_start;
+    int ryaxis_start;
+
+    int xaxis_xsize;
+    int xaxis_xstart;
+    int xaxis_ystart;
+    int xaxis_ysize;
+
+    // no of lines in status bar
+    uint32_t status_size;
+};
+
+void pl_print(Plot* pl);
+Cell* pl_get_cell(Plot* pl, uint32_t x, uint32_t y);
+void pl_draw_candlestick(Plot* pl, uint32_t ix, int32_t iopen, int32_t ihigh, int32_t ilow, int32_t iclose);
+void pl_draw_candlesticks(Plot* pl, Group* g, double dmin, double dmax, int32_t yoffset);
+void pl_clear_cells(Plot* pl);
+void pl_draw_ryaxis(Plot* pl, double dmin, double dmax, int32_t yoffset);
+void pl_draw_last_data(Plot* pl, double dmin, double dmax, double pany, double lasty);
+void pl_draw_xaxis(Plot* pl, Group* g);
+void pl_set_dimensions(Plot* pl, double dmin, double dmax);
+void pl_draw(Plot* pl, Index* index, Groups* groups, State* s);
+
+Plot* pl_init(uint32_t xsize, uint32_t ysize);
+Cell* cell_init();
+
+void get_tickerstr(char* buf, double ticker, uint32_t ntotal, uint32_t nwhole, uint32_t nfrac);
+
+#endif
