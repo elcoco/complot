45c859f16be11bea11bc85472ccf985eaeabb35c
eco <spambaconspam@protonmail.com>
2022-03-04 11:56:20 +0100

    moved defines to config.h

---
+++ src/config.h
@@ -0,0 +1,25 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define INDEX_DEFAULT_GROW_AMOUNT 2000
+//#define INDEX_DEFAULT_SPREAD 5
+
+#define LINE_LINE_ICON        "█"
+#define LINE_OHLC_ICON        "┿"
+#define LINE_DEFAULT_LINE_CHR "█"
+
+#define PLOT_MIN_WINDOW_XSIZE 10
+#define PLOT_MIN_WINDOW_YSIZE 10
+
+#define LEGEND_MAX_SIZE 200
+
+#define XAXIS_TICK_SPACING 15
+
+#define YAXIS_LINE_CHR "─"
+#define YAXIS_OHLC_BODY "█"
+#define YAXIS_OHLC_WICK "┃"
+
+
+
+
+#endif
